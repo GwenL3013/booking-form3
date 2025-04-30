d97618ca8db6865ddfbadbee84634b6d49f50777
GwenL3013 <gwenck2@gmail.com>
2025-04-30 11:47:14 +0800

    fix tour cards mobile view

--- src/components/todo/TodoCard.jsx
+++ src/components/todo/TodoCard.jsx
@@ -7,12 +7,11 @@ export default function TodoCard({ todo, onEdit }) {
     const completed = todo.completed;
     const border = completed ? "success" : "danger";
 
-
     const [timer, setTimer] = useState(0);
     const [timerInterval, setTimerInterval] = useState(null);
     const setTodos = useContext(TodoContext).setTodos;
 
-    //Functions related to timer
+    // Functions related to timer
     const startTimer = () => {
         if (timerInterval === null) {
             const intervalID = setInterval(() => {
@@ -31,11 +30,10 @@ export default function TodoCard({ todo, onEdit }) {
         clearInterval(timerInterval);
         setTimerInterval(null);
         setTimer(0);
-
     }
 
     const deleteTodo = () => {
-        const isConfirmed = window.confirm("Are you sure you want to delete this Todo?")
+        const isConfirmed = window.confirm("Are you sure you want to delete this Todo?");
 
         if (isConfirmed) {
             setTodos((prevTodos) =>
@@ -50,42 +48,37 @@ export default function TodoCard({ todo, onEdit }) {
         return () => {
             clearInterval(timerInterval);
         };
-    }, [timerInterval])
+    }, [timerInterval]);
 
     const cardBackgroundColor = completed ? '#630ed1d5' : '#f68914d5';
-
 
     return (
         <>
             <Card border={border} className="todo_card" style={{ backgroundColor: cardBackgroundColor }}>
-
                 <Card.Header>{!completed && "Not"} Completed</Card.Header>
-
                 <Card.Body>
                     <Card.Title className="todo_title">{todo.title}</Card.Title>
                     <Card.Text className="todo_text">{todo.description}</Card.Text>
                     <p>Timer: {timer} seconds</p>
-                    <Button onClick={startTimer}>
-                        <i className="bi bi-play"></i>
-                    </Button>
-                    <Button onClick={pauseTimer} className="mx-2">
-                        <i className="bi bi-pause-fill"></i>
-                    </Button>
-                    <Button onClick={resetTimer} className="mx-2">
-                        <i className="bi bi-arrow-clockwise"></i>
-                    </Button>
-                    <Button variant="secondary" onClick={onEdit} className="mx-2">
-                        <i className="bi bi-pencil"></i>
-                    </Button>
-
-                    <Button variant="danger" onClick={deleteTodo} className="mx-2">
-                        <i className="bi bi-trash3"></i>
-                    </Button>
-
-
-
+                    <div className="todo-buttons">
+                        <Button onClick={startTimer} className="todo-btn">
+                            <i className="bi bi-play"></i>
+                        </Button>
+                        <Button onClick={pauseTimer} className="todo-btn">
+                            <i className="bi bi-pause-fill"></i>
+                        </Button>
+                        <Button onClick={resetTimer} className="todo-btn">
+                            <i className="bi bi-arrow-clockwise"></i>
+                        </Button>
+                        <Button variant="secondary" onClick={onEdit} className="todo-btn">
+                            <i className="bi bi-pencil"></i>
+                        </Button>
+                        <Button variant="danger" onClick={deleteTodo} className="todo-btn">
+                            <i className="bi bi-trash3"></i>
+                        </Button>
+                    </div>
                 </Card.Body>
-            </Card >
+            </Card>
         </>
     );
 }
